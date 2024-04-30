44e8ad5f204298a7e8c95b03a2baf4bd5751d4c1
Alejandro <aleckbeu@gmail.com>
2024-04-30 17:27:51 +0000

    Alejandro

--- src/app.py
+++ src/app.py
@@ -10,15 +10,17 @@ def hello_world():
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
     request_body = request.json
-    print("Incoming requACest with the following body", request_body)
+    print("Incoming request with the following body", request_body)
     todos.append(request_body)
     return jsonify(todos)
 
 @app.route('/todos/<int:position>', methods=['DELETE'])
 def delete_todo(position):
     print("This is the position to delete:", position)
-    return 'something'
-
+    
+    todos.pop(position)
+    
+    return jsonify(todos)
 
 # These two lines should always be at the end of your app.py file
 if __name__ == '__main__':
